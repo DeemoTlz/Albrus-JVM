6e1f910382ae03b0ffdfd2b4a4518031fe415f06
DeemoTlz <825532516@qq.com>
2022-07-11 21:32:14 +0800

    JVM Learn project: JVM 垃圾收集器之 G1

--- src/main/java/com/albrus/gc/AlbrusGC.java
+++ src/main/java/com/albrus/gc/AlbrusGC.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class AlbrusGC {
 
+    /**
+     * -Xms10m -Xmx10m -XX:+PrintGCDetails
+     * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+UseSerialGC
+     * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+UseParallelGC
+     * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+UseConcMarkSweepGC
+     * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:UseG1GC
+     */
     public static void main(String[] args) throws InterruptedException {
         // -Xms10m -Xmx10m -XX:+PrintGCDetails
         // byte[] bytes = new byte[100 * 1024 * 1024];
